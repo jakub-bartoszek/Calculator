81cfad71be8c1bdf5f8f44f05f23a5140dc9dfea
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-09-17 18:04:38 +0200

    added styles for calculator

---
+++ src/features/Calculator/styled.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+	margin: 100px auto;
+	width: max-content;
+	background-color: #00000050;
+	box-shadow: 0 10px 30px #00000070;
+`;
+
