894e480fac22f560dd33d317fc6603943bcfdd41
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-09-19 15:32:17 +0200

    removed normalize

--- src/core/App/index.jsx
+++ src/core/App/index.jsx
@@ -1,10 +1,8 @@
 import { Calculator } from "../../features/Calculator";
 import { GlobalStyle } from "./GlobalStyle";
-import { Normalize } from "styled-normalize";
 
 const App = () => (
 	<>
-		<Normalize />
 		<GlobalStyle />
 		<Calculator />
 	</>
